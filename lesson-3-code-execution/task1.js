0ac33bbd79a8f032842294f246e240af5fd9b055
MIKE <mikhail.khakalo@gmail.com>
2022-02-10 23:39:51 +0300

    First task

---
+++ lesson-3-code-execution/task1.js
@@ -0,0 +1,21 @@
+var tasksCompleted = {
+    'Anna': 29,
+    'Serg': 35,
+    'Elena': 1,
+    'Anton': 99,
+    'Maks': 123,
+    'Lolita' : 32,
+    'Inna' : 23432,
+}
+function biggest (obj) {
+    var result = 0;
+    var resultName ='';
+    for (key in obj) {
+        if (result < obj[key]) {
+            result = obj[key];
+            resultName = key;
+        }
+    }
+    return resultName;
+}
+console.log(biggest(tasksCompleted));
